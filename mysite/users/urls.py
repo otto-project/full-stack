61d64c7fa965aa3e4b0993637e93c0035ac18aeb
1000zoo <cjswldn99@gmail.com>
2024-07-25 15:41:39 +0900

    feat: login 및 logout 간단하게 구현

--- mysite/users/urls.py
+++ mysite/users/urls.py
@@ -5,6 +5,7 @@ from . import views as users_view
 
 app_name='users'
 urlpatterns = [
-    path('', auth_view.LoginView.as_view(template_name='users/login.html'), name="login"),
+    path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name="login"),
+    path('logout/', auth_view.LogoutView.as_view(next_page='/'), name="logout"),
     path('signup/', users_view.signup, name='signup')
 ]
