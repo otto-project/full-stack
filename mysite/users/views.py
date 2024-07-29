c655c2c136fd74dc75835d4fd8bbd05004721906
1000zoo <cjswldn99@gmail.com>
2024-07-29 09:52:43 +0900

    Feat: 로그인 및 회원가입 시 잘못된 입력이 들어오면 에러

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,10 +1,37 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.contrib.auth import views as auth_views
 from django.shortcuts import redirect
+from django.shortcuts import render
+
 from .forms import SignUpForm
+
+
+class CustomLoginView(auth_views.LoginView):
+    template_name = 'users/login.html'
+
+    def form_invalid(self, form):
+        messages.error(self.request, '사용자ID 또는 비밀번호가 잘못되었습니다.')
+        return redirect('users:login')
+
 
 # Create your views here.
 def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        print(username)
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        if user is not None:
+            login(request, user)
+            return redirect('main')  # 로그인 후 이동할 페이지
+        else:
+            messages.error(request, '사용자ID 또는 비밀번호가 잘못되었습니다.')
+
     return render(request, 'users/login.html')
+
 
 def signup(request):
     if request.method == 'POST':
