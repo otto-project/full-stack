e621860a393af1fad07d4472ce15d01ddeab5d29
SuyeonYun <yusuyeon678@gmail.com>
2024-07-29 12:55:00 +0900

    Feat: Add post-login redirection and immediate login after signup

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = "/staticfiles/"
+STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -129,4 +129,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
--- mysite/users/views.py
+++ mysite/users/views.py
@@ -8,21 +8,21 @@ from .forms import SignUpForm
 
 
 class CustomLoginView(auth_views.LoginView):
-    template_name = 'users/login.html'
+    template_name = "users/login.html"
 
     def form_invalid(self, form):
-        messages.error(self.request, '사용자ID 또는 비밀번호가 잘못되었습니다.')
-        return redirect('users:login')
+        messages.error(self.request, "사용자ID 또는 비밀번호가 잘못되었습니다.")
+        return redirect("users:login")
 
 
 def signup(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            messages.success(request, '회원가입 성공!')
-            return redirect('main')
+            messages.success(request, "회원가입 성공!")
+            return redirect("main")
     else:
         form = SignUpForm()
-    return render(request, 'users/signup.html', {'form': form})
+    return render(request, "users/signup.html", {"form": form})
