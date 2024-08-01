a580d0c364dbf3cc8ae73ebec68a7c9e24f47b74
1000zoo <cjswldn99@gmail.com>
2024-08-01 18:08:44 +0900

    Chore: ?

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+from products.models import ProductTable
+
 
 # Create your views here.
 def index(request):
     return render(request, "main/main.html")
+
+
+def musinsa_main(request):
+    products = ProductTable.objects.all()[:10]
+    return render(request, "main/musinsa_main.html")
