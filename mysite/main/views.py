bc6ca27cafe01af7618d0acf5d27850c894b5cdf
1000zoo <cjswldn99@gmail.com>
2024-08-02 10:49:43 +0900

    Refactor: main html 로그인/로그아웃 페이지 분리

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -9,10 +9,9 @@ def index(request):
 
 
 def main_platform(request, platform='musinsa'):
-    if request.user.is_authenticated:
-        html = 'main/logged_in.html'
-    else:
-        html = 'main/logged_out.html'
     top = ProductTable.get_product_order_by_rank(platform, 'top')[:10]
     bottom = ProductTable.get_product_order_by_rank(platform, 'bottom')[:10]
-    return render(request, html, {'top': top, 'bottom': bottom})
+    if request.user.is_authenticated:
+        return render(request, 'main/logged_in.html', {'top': top, 'bottom': bottom})
+    else:
+        return render(request, 'main/logged_out.html', {'top': top, 'bottom': bottom})
