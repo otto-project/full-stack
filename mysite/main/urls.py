df4c3771b4ba965f6f2682a7264be2418158e689
1000zoo <cjswldn99@gmail.com>
2024-08-02 10:40:45 +0900

    Refactor: main html 로그인/로그아웃 페이지 분리

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from . import views as main_view
 
 urlpatterns = [
-    path('', main_view.main_musinsa, name="main"),
-    path('musinsa/', main_view.main_musinsa, name="main_musinsa"),
-    path('29cm/', main_view.main_29cm, name="main_29cm"),
-    path('zigzag/', main_view.main_zigzag, name="main_zigzag"),
+    path('', main_view.index, name='index'),
+    path('<str:platform>/', main_view.main_platform, name='main_platform'),
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,26 +1,18 @@
+from django.shortcuts import redirect
 from django.shortcuts import render
-
 from products.models import ProductTable
 
 
 # Create your views here.
 def index(request):
-    return render(request, "main/main.html")
+    return redirect('main_platform', platform='musinsa')
 
 
-def main_musinsa(request):
-    top = ProductTable.get_product_order_by_rank('musinsa', 'top')[:10]
-    bottom = ProductTable.get_product_order_by_rank('musinsa', 'bottom')[:10]
-    return render(request, "main/main.html", {'top': top, 'bottom': bottom})
-
-
-def main_29cm(request):
-    top = ProductTable.get_product_order_by_rank('29cm', 'top')[:10]
-    bottom = ProductTable.get_product_order_by_rank('29cm', 'bottom')[:10]
-    return render(request, "main/main.html", {'top': top, 'bottom': bottom})
-
-
-def main_zigzag(request):
-    top = ProductTable.get_product_order_by_rank('zigzag', 'top')[:10]
-    bottom = ProductTable.get_product_order_by_rank('zigzag', 'bottom')[:10]
-    return render(request, "main/main.html", {'top': top, 'bottom': bottom})
+def main_platform(request, platform='musinsa'):
+    if request.user.is_authenticated:
+        html = 'main/logged_in.html'
+    else:
+        html = 'main/logged_out.html'
+    top = ProductTable.get_product_order_by_rank(platform, 'top')[:10]
+    bottom = ProductTable.get_product_order_by_rank(platform, 'bottom')[:10]
+    return render(request, html, {'top': top, 'bottom': bottom})
