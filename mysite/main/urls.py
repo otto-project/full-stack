dd71fdd33b49322f72d47c509d5ebf9a74ab0a2c
1000zoo <cjswldn99@gmail.com>
2024-08-02 10:05:37 +0900

    Feat: 플랫폼 별 표시되는 제품 분리

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views as main_view
 
 urlpatterns = [
-    path('', main_view.index, name="main")
+    path('', main_view.main_musinsa, name="main"),
+    path('musinsa/', main_view.main_musinsa, name="main_musinsa"),
+    path('29cm/', main_view.main_29cm, name="main_29cm"),
+    path('zigzag/', main_view.main_zigzag, name="main_zigzag"),
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -5,9 +5,22 @@ from products.models import ProductTable
 
 # Create your views here.
 def index(request):
-    products = ProductTable.objects.all()[:10]
-    return render(request, "main/main.html", {'products': products})
+    return render(request, "main/main.html")
 
 
-def musinsa_main(request):
-    return render(request, "main/musinsa_main.html")
+def main_musinsa(request):
+    top = ProductTable.get_product_order_by_rank('musinsa', 'top')[:10]
+    bottom = ProductTable.get_product_order_by_rank('musinsa', 'bottom')[:10]
+    return render(request, "main/main.html", {'top': top, 'bottom': bottom})
+
+
+def main_29cm(request):
+    top = ProductTable.get_product_order_by_rank('29cm', 'top')[:10]
+    bottom = ProductTable.get_product_order_by_rank('29cm', 'bottom')[:10]
+    return render(request, "main/main.html", {'top': top, 'bottom': bottom})
+
+
+def main_zigzag(request):
+    top = ProductTable.get_product_order_by_rank('zigzag', 'top')[:10]
+    bottom = ProductTable.get_product_order_by_rank('zigzag', 'bottom')[:10]
+    return render(request, "main/main.html", {'top': top, 'bottom': bottom})
