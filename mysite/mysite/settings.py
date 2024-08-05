6cc41d05b81a9dce1c7ccf113a13c96d33ddbdaf
culonculon <culonculon48@gmail.com>
2024-08-05 17:36:15 +0900

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -76,16 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "mysite.wsgi.application"
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
