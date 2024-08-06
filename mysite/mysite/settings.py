7613e2c860777ed3e38688da254900fb237db809
culonculon <culonculon48@gmail.com>
2024-08-06 13:14:16 +0900

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -149,3 +149,7 @@ else:
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
