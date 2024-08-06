8298388fae50fa3b1f074d2b252f01ad8354bfa2
1000zoo <cjswldn99@gmail.com>
2024-08-06 17:06:43 +0900

    Feat: 성별별로 제품을 필터링 적용

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -9,10 +9,16 @@ def index(request):
 
 
 def main_platform(request, platform='musinsa'):
-    top = ProductTable.get_product_order_by_rank(platform, 'top')[:10]
-    bottom = ProductTable.get_product_order_by_rank(platform, 'bottom')[:10]
     if request.user.is_authenticated:
         gender = request.user.gender
+        top = ProductTable.get_product_filter_by_gender(platform, 'top', gender)[:10]
+        bottom = ProductTable.get_product_filter_by_gender(platform, 'bottom', gender)[:10]
         return render(request, 'main/logged_in.html', {'top': top, 'bottom': bottom, 'gender': gender})
     else:
-        return render(request, 'main/logged_out.html', {'top': top, 'bottom': bottom})
+        women_top = ProductTable.get_product_filter_by_gender(platform, 'top', 'women')[:10]
+        women_bottom = ProductTable.get_product_filter_by_gender(platform, 'bottom', 'women')[:10]
+        men_top = ProductTable.get_product_filter_by_gender(platform, 'top', 'men')[:10]
+        men_bottom = ProductTable.get_product_filter_by_gender(platform, 'bottom', 'men')[:10]
+        return render(request, 'main/logged_out.html',
+                      {'women_top': women_top, 'women_bottom': women_bottom, 'men_top': men_top,
+                       'men_bottom': men_bottom})
