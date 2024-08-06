2af7f6639fb30fcc9360fdbda363da146160147f
1000zoo <cjswldn99@gmail.com>
2024-08-06 18:11:37 +0900

    Fix: 로그아웃 시, 제품이 나오지 않는 오류 수정

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -3,22 +3,21 @@ from django.shortcuts import render
 from products.models import ProductTable
 
 
-# Create your views here.
 def index(request):
     return redirect('main_platform', platform='musinsa')
 
 
-def main_platform(request, platform='musinsa'):
+def main_platform(request, platform):
     if request.user.is_authenticated:
         gender = request.user.gender
         top = ProductTable.get_product_filter_by_gender(platform, 'top', gender)[:10]
         bottom = ProductTable.get_product_filter_by_gender(platform, 'bottom', gender)[:10]
         return render(request, 'main/logged_in.html', {'top': top, 'bottom': bottom, 'gender': gender})
     else:
-        women_top = ProductTable.get_product_filter_by_gender(platform, 'top', 'women')[:10]
-        women_bottom = ProductTable.get_product_filter_by_gender(platform, 'bottom', 'women')[:10]
-        men_top = ProductTable.get_product_filter_by_gender(platform, 'top', 'men')[:10]
-        men_bottom = ProductTable.get_product_filter_by_gender(platform, 'bottom', 'men')[:10]
+        women_top = ProductTable.get_product_filter_by_gender(platform, 'top', 'female')[:10]
+        women_bottom = ProductTable.get_product_filter_by_gender(platform, 'bottom', 'female')[:10]
+        men_top = ProductTable.get_product_filter_by_gender(platform, 'top', 'male')[:10]
+        men_bottom = ProductTable.get_product_filter_by_gender(platform, 'bottom', 'male')[:10]
         return render(request, 'main/logged_out.html',
                       {'women_top': women_top, 'women_bottom': women_bottom, 'men_top': men_top,
                        'men_bottom': men_bottom})
