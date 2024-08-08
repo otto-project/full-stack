abf1ca75b894f2663ea15d2b3427b7c115d09c4f
1000zoo <cjswldn99@gmail.com>
2024-08-08 14:40:10 +0900

    Feat: hover를 이용하여 마우스 오버 시, 제품 사이즈 표시 - 추후에 제품 사이즈를 추천 사이즈로 수정할 예정

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -28,7 +28,7 @@ LOGIN_REDIRECT_URL = "/"
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ["*"]
 AUTH_USER_MODEL = "users.CustomUser"
