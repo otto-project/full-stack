7fcf64c5c6098be29d070bc8232de584ac7b6777
1000zoo <cjswldn99@gmail.com>
2024-08-09 14:33:26 +0900

    Fix: rollback

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,5 +1,3 @@
-import requests
-from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import login
 from django.contrib.auth import views as auth_views
@@ -12,11 +10,11 @@ from .forms import SignUpForm
 class CustomLoginView(auth_views.LoginView):
     template_name = "users/login.html"
 
-    def form_valid(self, form):
-        host = settings.ML_API_HOST
-        api_url = f"http://{host}/predict?height=150&weight=10"
-        res = requests.get(api_url)
-        print(res)
+    # def form_valid(self, form):
+    #     host = settings.ML_API_HOST
+    #     api_url = f"http://{host}/predict?height=150&weight=10"
+    #     res = requests.get(api_url)
+    #     print(res)
 
     def form_invalid(self, form):
         messages.error(self.request, "사용자ID 또는 비밀번호가 잘못되었습니다.")
