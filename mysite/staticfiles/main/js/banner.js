66eb1771d560d003bb79a711697b00b30cd03785
SuyeonYun <yusuyeon678@gmail.com>
2024-08-16 10:19:56 +0900

    Fix: remove banner size function

--- mysite/staticfiles/main/js/banner.js
+++ mysite/staticfiles/main/js/banner.js
@@ -1,5 +1,4 @@
 const slide = document.querySelector('.slider-wrapper');
-let slideWidth = slide.clientWidth;
 
 const prevButton = document.querySelector('.prev-slide');
 const nextButton = document.querySelector('.next-slide');
@@ -54,9 +53,9 @@ for (let i = 0; i < maxSlide; i++) {
 }
 
 // 브라우저 화면이 조정될 때 마다 slideWidth를 변경하기 위해
-window.addEventListener("resize", () => {
-    slideWidth = slide.clientWidth;
-});
+// window.addEventListener("resize", () => {
+//     slideWidth = slide.clientWidth;
+// });
 
 function moveToNextSlide() {
     currSlide += 1;
