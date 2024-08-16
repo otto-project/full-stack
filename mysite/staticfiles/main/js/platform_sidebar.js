e3fa2354aef519d5eba5a7fd14410185026355c7
1000zoo <cjswldn99@gmail.com>
2024-08-16 12:32:59 +0900

    Feat: 플랫폼을 누르면 바로 제품이 보이게 수정

--- mysite/staticfiles/main/js/platform_sidebar.js
+++ mysite/staticfiles/main/js/platform_sidebar.js
@@ -1,17 +1,37 @@
 document.getElementById("musinsa").addEventListener("click", function() {
+    toggleContent();
     document.getElementById("musinsa").style.textDecoration = "underline";
     document.getElementById("29cm").style.textDecoration = "none";
     document.getElementById("zigzag").style.textDecoration = "none";
 });
 
 document.getElementById("29cm").addEventListener("click", function() {
+    toggleContent();
     document.getElementById("musinsa").style.textDecoration = "none";
     document.getElementById("29cm").style.textDecoration = "underline";
     document.getElementById("zigzag").style.textDecoration = "none";
 });
 
 document.getElementById("zigzag").addEventListener("click", function() {
+    toggleContent();
     document.getElementById("musinsa").style.textDecoration = "none";
     document.getElementById("29cm").style.textDecoration = "none";
     document.getElementById("zigzag").style.textDecoration = "underline";
 });
+
+function toggleContent() {
+    document.getElementById("home").style.display = "none";
+    var womenContent = document.getElementById("women-content");
+    var menContent = document.getElementById("men-content");
+
+    if (womenContent) {  // women-content가 존재할 경우
+        womenContent.style.display = "flex";
+    } else if (menContent) {  // men-content가 존재할 경우
+        menContent.style.display = "flex";
+    }
+}
+
+// 페이지 로드 시 toggleContent 함수 실행
+window.onload = function() {
+    toggleContent();
+}
