3411d4e0a2bc21cca49bf2213ced6a6b49d9d918
1000zoo <cjswldn99@gmail.com>
2024-08-16 23:05:32 +0900

    Style: 플랫폼 클릭시 밑줄 추가

--- mysite/staticfiles/main/js/platform_sidebar.js
+++ mysite/staticfiles/main/js/platform_sidebar.js
@@ -1,25 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
     toggleContent();
-    document.getElementById("musinsa").addEventListener("click", function() {
-        document.getElementById("musinsa").style.textDecoration = "underline";
-        document.getElementById("29cm").style.textDecoration = "none";
-        document.getElementById("zigzag").style.textDecoration = "none";
-    });
-});
-document.addEventListener("DOMContentLoaded", function() {
-    toggleContent();
-    document.getElementById("29cm").addEventListener("click", function() {
-        document.getElementById("musinsa").style.textDecoration = "none";
-        document.getElementById("29cm").style.textDecoration = "underline";
-        document.getElementById("zigzag").style.textDecoration = "none";
-    });
-});
-document.addEventListener("DOMContentLoaded", function() {
-    toggleContent();
-    document.getElementById("zigzag").addEventListener("click", function() {
-        document.getElementById("musinsa").style.textDecoration = "none";
-        document.getElementById("29cm").style.textDecoration = "none";
-        document.getElementById("zigzag").style.textDecoration = "underline";
+
+    const links = document.querySelectorAll(".sidebar ul li a");
+
+    // 페이지 로드 시 저장된 상태를 확인하고 밑줄을 추가
+    const activeLinkId = localStorage.getItem('activeLink');
+    if (activeLinkId) {
+        document.getElementById(activeLinkId).style.textDecoration = "underline";
+    }
+
+    links.forEach(function(link) {
+        link.addEventListener("click", function() {
+            // 모든 링크의 밑줄 제거
+            links.forEach(function(link) {
+                link.style.textDecoration = "none";
+            });
+            // 클릭한 링크에 밑줄 추가
+            this.style.textDecoration = "underline";
+            // 클릭한 링크의 ID를 localStorage에 저장
+            localStorage.setItem('activeLink', this.id);
+        });
     });
 });
 
@@ -27,16 +27,15 @@ function toggleContent() {
     var womenContent = document.getElementById("women-content");
     var menContent = document.getElementById("men-content");
 
-    if (womenContent) {  // women-content가 존재할 경우
+    if (womenContent) {
         document.getElementById("home").style.display = "none";
         womenContent.style.display = "flex";
-    } else if (menContent) {  // men-content가 존재할 경우
+    } else if (menContent) {
         document.getElementById("home").style.display = "none";
         menContent.style.display = "flex";
     }
 }
 
-// 페이지 로드 시 toggleContent 함수 실행
 window.onload = function() {
     toggleContent();
 }
